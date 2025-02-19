acb77243c53778abfb816615f17f39e4d01a27b5
Dewmo123 <akh200808@gmail.com>
2025-02-19 23:08:37 +0900

    .add invendata

--- Assets/0.Work/Dewmo123/Scripts/Items/ToolItemDataSO.cs
+++ Assets/0.Work/Dewmo123/Scripts/Items/ToolItemDataSO.cs
@@ -1,6 +1,8 @@
 ﻿using Agama.Scripts.Combats;
+using Agama.Scripts.Core;
 using Agama.Scripts.Entities;
 using Scripts.Items;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets._0.Work.Dewmo123.Scripts.Items
@@ -10,9 +12,23 @@ namespace Assets._0.Work.Dewmo123.Scripts.Items
     public class ToolItemDataSO : ItemDataSO, IUsable
     {
         public IDamageable.DamageMethodType damageType;
+        public List<AddingStat> addingStats;
+        public void AddModifier(EntityStat statCompo)
+        {
+            foreach (AddingStat stat in addingStats)
+            {
+                StatSO targetStat = statCompo.GetStat(stat.targetStat);
+                if (targetStat != null)
+                {
+                    targetStat.AddModifier(itemName, stat.modifyValue);
+                }
+            }
+        }
+
 
         public void ChoiceItem(Entity entity)
         {
+            AddModifier(entity.GetComp<EntityStat>());
         }
 
         public void UseItem(Entity entity)
