796e51caa339c058eafa0651a0a9163f767cf7db
machany <machanicaly@gmail.com>
2025-02-23 00:20:58 +0900

    fea : BT Change Event

--- Assets/0.Work/Agama/Scripts/Enemies/Sheep.cs
+++ Assets/0.Work/Agama/Scripts/Enemies/Sheep.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Unity.Behavior;
 
 namespace Agama.Scripts.Enemies
 {
     public class Sheep : BehaviorEnemy
     {
+        [BlackboardEnum]
+        public enum SheepState
+        {
+            Patroll,
+            Run,
+            Hit,
+            Dead
+        }
+
         protected override void HandleHitEvent()
         {
         }
