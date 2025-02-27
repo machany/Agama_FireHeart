d9c61c65e23c9e4f53157a41d055f81c5eec55fa
Dewmo123 <akh200808@gmail.com>
2025-02-28 01:08:33 +0900

    .fix canvas

--- Assets/0.Work/Agama/Scripts/Players/States/PlayerDeadState.cs
+++ Assets/0.Work/Agama/Scripts/Players/States/PlayerDeadState.cs
@@ -2,13 +2,17 @@
 using Agama.Scripts.Entities;
 using Agama.Scripts.Entities.FSM;
 using System;
+using UnityEngine.SceneManagement;
 
 namespace Agama.Scripts.Players.States
 {
     public class PlayerDeadState : EntityState, IEventState
     {
+
+        private EntityAnimatorTrigger _trigger;
         public PlayerDeadState(Entity owner, AnimationParamiterSO animationParamitor) : base(owner, animationParamitor)
         {
+            _trigger = _owner.GetComp<EntityAnimatorTrigger>();
             _mover = _owner.GetComp<EntityMover>();
         }
 
@@ -19,13 +23,20 @@ namespace Agama.Scripts.Players.States
         public override void Enter()
         {
             base.Enter();
+            _trigger.OnAnimationEndEvent += HandleAnimationEnd;
             _mover.CanMove = false;
             _mover.StopImmediately();
             (_owner as Player).SetStateChangeLock(true);
         }
 
+        private void HandleAnimationEnd()
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+
         public override void Exit()
         {
+            _trigger.OnAnimationEndEvent -= HandleAnimationEnd;
             _mover.CanMove = true;
             (_owner as Player).SetStateChangeLock(false);
 
