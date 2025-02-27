05fdde9ee24b196782a62a490d7d9a95a9a4ee7f
Dewmo123 <akh200808@gmail.com>
2025-02-28 03:49:49 +0900

    .

---
+++ Assets/0.Work/Dewmo123/Scripts/Utiles/MovingSortingLayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scripts.Utiles
+{
+    public class MovingSortingLayer : MonoBehaviour
+    {
+        private SpriteRenderer _renderer;
+        [SerializeField] private int _offset;
+        private void Awake()
+        {
+            _renderer = transform.GetComponent<SpriteRenderer>();
+            _renderer.sortingOrder = Mathf.RoundToInt(transform.parent.position.y * -100) + _offset;
+        }
+        private void Update()
+        {
+            _renderer.sortingOrder = Mathf.RoundToInt(transform.parent.position.y * -100) + _offset;
+        }
+    }
+}
